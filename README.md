982153e10baa30f97f714c9c51e857b198c1d182
Nathan <308719298@qq.com>
2018-06-12 14:17:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple-koa-redis-session
---
+++ index.js
@@ -0,0 +1,44 @@
+const ioredis = require('ioredis')
+const uuid = require('uuid')
+const crypto = require('crypto')
+
+const md5 = str => {
+  crypto
+    .createHash('md5')
+    .update(str)
+    .digest()
+}
+
+module.exports = ({ ioredisOptions, maxAge, sessionId }) => {
+  const redis = new ioredis(ioredisOptions)
+
+  redis.on('connect', () => {
+    console.log('redis connected...')
+  })
+
+  redis.on('error', error => {
+    throw error
+  })
+
+  return async function redisSeesionMiddleware(ctx, next) {
+    ctx.$storeUserInfo = userInfo => {
+      let cookieVal = md5(Date.now() + uuid() + 'bestnathan')
+      return redis
+        .set(cookieVal, JSON.stringify(userInfo))
+        .then(() => {
+          return redis.expire(cookieVal, maxAge)
+        })
+        .then(() => {
+          ctx.cookies.set(sessionId, cookieVal, { maxAge: maxAge * 1000 })
+        })
+    }
+
+    ctx.$getUserInfo = () => {
+      let cookieVal = ctx.cookies.get(sessionId)
+      if (!cookieVal) {
+        return Promise.reject('cookie value is not exsit!')
+      }
+      return redis.get(cookieVal).then(res => JSON.parse(res))
+    }
+  }
+}
